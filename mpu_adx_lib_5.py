6ab624e5395d1239fc17c344b22ecb0577fb71c1
AntonioGS <96085519+kaansau@users.noreply.github.com>
2022-08-05 22:28:36 -0500

    Update mpu_adx_lib_5.py

--- mpu_adx_lib_5.py
+++ mpu_adx_lib_5.py
@@ -1,134 +1,130 @@
-import time
-from time import sleep
-import pickle
+import time                  #libreria para ocupar funciones de tiempo
+from time import sleep       #Modulo para impementar retardos 
+import pickle                #libreria para serializar objetos 
 
-import board
-import adafruit_adxl34x
+import board                 #libreria para manejar el protocolo i2c 
+import adafruit_adxl34x      #libreria para leer el sensor adxl 
 
-import json
+import json                  #libreria para manejar cadenas de ddatos en formato json  
 
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
+import sys                   #libreria para tener acceso a funciones del sistema 
+import numpy as np           #libreria para manipular datos en forma de arreglos o matrices
+import matplotlib.pyplot as plt    #libreria para implementar gráficos
 
-import paho.mqtt.client as paho
-broker="broker.emqx.io"
-port=1883
+import paho.mqtt.client as paho     #libreria para implementar el protocolo mqtt
+broker="broker.emqx.io"             #definición del bloker publico
+port=1883                           #puerto de acceso al broker
 
-from mpu6050 import mpu6050
-sensor1 = mpu6050(0x69)
-sensor2 = mpu6050(0x68)
+from mpu6050 import mpu6050         #libreria para leer el sensor mpu6050   
+sensor1 = mpu6050(0x69)             #definición de un objeto del sensor mpu6050 con direccion 0x69
+sensor2 = mpu6050(0x68)             #definición de un objeto del sensor mpu6050 con direccion 0x68 
 
-v_1 = np.array([0.0])
-v_2 = np.array([0.0])
-v_3 = np.array([0.0])
-v_4 = np.array([0.0])
-v_5 = np.array([0.0])
-v_6 = np.array([0.0])
-v_7 = np.array([0.0])
-v_8 = np.array([0.0])
-v_9 = np.array([0.0])
-frecuenciahz = np.array([0.0])
+v_1 = np.array([0.0])               #definición de arreglo para almacenar datos de la posicion x sensor 1
+v_2 = np.array([0.0])               #definición de arreglo para almacenar datos de la posicion y sensor 1
+v_3 = np.array([0.0])               #definición de arreglo para almacenar datos de la posicion z sensor 1
+v_4 = np.array([0.0])               #definición de arreglo para almacenar datos de la posicion x sensor 2
+v_5 = np.array([0.0])               #definición de arreglo para almacenar datos de la posicion y sensor 2
+v_6 = np.array([0.0])               #definición de arreglo para almacenar datos de la posicion z sensor 2
+v_7 = np.array([0.0])               #definición de arreglo para almacenar datos de la posicion x sensor 3
+v_8 = np.array([0.0])               #definición de arreglo para almacenar datos de la posicion y sensor 3
+v_9 = np.array([0.0])               #definición de arreglo para almacenar datos de la posicion z sensor 3
+frecuenciahz = np.array([0.0])      #definición de arreglo para almacenar el historial de datos de frecuencias  
 
-i2c = board.I2C()
-acelero = adafruit_adxl34x.ADXL345(i2c)
+i2c = board.I2C()                            #definicón del objeto para manejar el protocolo i2c
+acelero = adafruit_adxl34x.ADXL345(i2c)      #definición de un objeto del sensor adxl  
 
-def on_publish(client,userdata,result):             #create function for callback
+def on_publish(client,userdata,result):      #definición de la función para publicar en el broker a traves de mqtt
     print("data published \n")
     pass
 
 
-def Calibracion(acel):
-    x_ac = 0
+def Calibracion(acel):                        #definición de la función de calibracion para el sensor mpu6050
+    x_ac = 0                                  #valores iniciales de las variables x, y y z
     y_ac = 0
     z_ac = 0
 
-    for x in range(2):
+    for x in range(2):                        #se toma la lectura de 2 valores para establecer un promedio  
         #print(y_ac)
         x_ac = x_ac + acel.get('x')
         y_ac = y_ac + acel.get('y')
         z_ac = z_ac + acel.get('z')
     
-    x_p = x_ac/2
+    x_p = x_ac/2                              #se toma el promedio de las lecturas del acelerometro en los ejes x, y, z
     y_p = y_ac/2
     z_p = z_ac/2
  
-    return(x_p, y_p, z_p)
+    return(x_p, y_p, z_p)                     #Retorno de los valores x, y, z
 
-def cal_adx():
+def cal_adx():                                 #Función de calibracion para el sensor adxl
 
-    x_ac_adx = 0
+    x_ac_adx = 0                               #valores iniciales de las variables x, y y z
     y_ac_adx = 0
     z_ac_adx = 0
     for x in range(2):
-        #acelero = adafruit_adxl34x.ADXL345(i2c)
+                                                                     #se toma la lectura de 2 valores para establecer un promedio 
         x_ac_adx_d, y_ac_adx_d, z_ac_adx_d = acelero.acceleration
         x_ac_adx = x_ac_adx + x_ac_adx_d
         y_ac_adx = y_ac_adx + y_ac_adx_d
         z_ac_adx = z_ac_adx + z_ac_adx_d
         
-    x_p_adx = x_ac_adx/2
+    x_p_adx = x_ac_adx/2                                             #se toma el promedio de las lecturas del acelerometro en los ejes x, y, z
     y_p_adx = y_ac_adx/2
     z_p_adx = z_ac_adx/2
 
-    return(x_p_adx, y_p_adx, z_p_adx)
+    return(x_p_adx, y_p_adx, z_p_adx)          #Retorno de los valores x, y, z
 
-while True:
+while True:                                    #comienzo del bucle principal
     
-    contador = 0
-    base_temp = time.time()
-    while contador < 799:
-        #base_temp = time.time()
-        of_x1, of_y1, of_z1 = Calibracion(sensor1.get_accel_data())
-        of_x2, of_y2, of_z2 = Calibracion(sensor2.get_accel_data())
-        of_x3, of_y3, of_z3 = cal_adx()
+    contador = 0                               #contador de toma de muesgtras
+    base_temp = time.time()                    #se realiza la medición del tiempo actual 
+    while contador < 799:                      #mientras el numero de muestras tomado sea menor a 799  
+        #base_temp = time.time()               #se realiza la medición del tiempo actual para determinar la duración de un ciclo de muestreo      
+        of_x1, of_y1, of_z1 = Calibracion(sensor1.get_accel_data())          #eliminacion del offset del sensor 1
+        of_x2, of_y2, of_z2 = Calibracion(sensor2.get_accel_data())          #eliminacion del offset del sensor 2
+        of_x3, of_y3, of_z3 = cal_adx()                                      #eliminacion del offset del sensor 3
 
 
-        acele1 = sensor1.get_accel_data()
-        x_a_1=acele1.get('x')-of_x1
-        #y_a_1=acele1.get('y')-of_y1
-        #z_a_1=acele1.get('z')-of_z1
+        acele1 = sensor1.get_accel_data()                                    #toma de lecturas del sensor 1
+        x_a_1=acele1.get('x')-of_x1                                          #aceleración en x del sensor 1
+        #y_a_1=acele1.get('y')-of_y1                                         #aceleración en y del sensor 1
+        #z_a_1=acele1.get('z')-of_z1                                         #aceleración en z del sensor 1
 
-        acele2 = sensor2.get_accel_data()
-        x_a_2=acele2.get('x')-of_x2
-        #y_a_2=acele2.get('y')-of_y2
-        #z_a_2=acele2.get('z')-of_z2
+        acele2 = sensor2.get_accel_data()                                    #toma de lecturas del sensor 2
+        x_a_2=acele2.get('x')-of_x2                                          #aceleración en x del sensor 2
+        #y_a_2=acele2.get('y')-of_y2                                         #aceleración en y del sensor 2
+        #z_a_2=acele2.get('z')-of_z2                                         #aceleración en z del sensor 2
     
-        x_ac_adx_d, y_ac_adx_d, z_ac_adx_d = acelero.acceleration
+        x_ac_adx_d, y_ac_adx_d, z_ac_adx_d = acelero.acceleration            #toma de lecturas del sensor 3
+        x_a_3=x_ac_adx_d-of_x3                                               #aceleración en x del sensor 3                                              
+        #y_a_3=y_ac_adx_d-of_y3                                              #aceleración en y del sensor 3
+        #z_a_3=z_ac_adx_d-of_z3                                              #aceleración en z del sensor 3
         
-        x_a_3=x_ac_adx_d-of_x3
+        v_1 = np.append(v_1, x_a_1)                                          #almacenamiento en un vector de la aceleración en x del sensor 1
+        #v_2 = np.append(v_2, y_a_1)                                         #almacenamiento en un vector de la aceleración en y del sensor 1 
+        #v_3 = np.append(v_3, z_a_1)                                         #almacenamiento en un vector de la aceleración en z del sensor 1
+        v_4 = np.append(v_4, x_a_2)                                          #almacenamiento en un vector de la aceleración en x del sensor 2
+        #v_5 = np.append(v_5, y_a_2)                                         #almacenamiento en un vector de la aceleración en y del sensor 2
+        #v_6 = np.append(v_6, z_a_2)                                         #almacenamiento en un vector de la aceleración en z del sensor 2
+        v_7 = np.append(v_7, x_a_3)                                          #almacenamiento en un vector de la aceleración en x del sensor 3
+        #v_8 = np.append(v_8, y_a_3)                                         #almacenamiento en un vector de la aceleración en y del sensor 3
+        #v_9 = np.append(v_9, z_a_3)                                         #almacenamiento en un vector de la aceleración en z del sensor 3
         
-        #y_a_3=y_ac_adx_d-of_y3
-        #z_a_3=z_ac_adx_d-of_z3
-        
-        v_1 = np.append(v_1, x_a_1)
-        #v_2 = np.append(v_2, y_a_1)
-        #v_3 = np.append(v_3, z_a_1)
-        v_4 = np.append(v_4, x_a_2)
-        #v_5 = np.append(v_5, y_a_2)
-        #v_6 = np.append(v_6, z_a_2)
-        v_7 = np.append(v_7, x_a_3)
-        #v_8 = np.append(v_8, y_a_3)
-        #v_9 = np.append(v_9, z_a_3)
-        #v_7 = np.append(v_7, z_a_3)
-        
-        
-        #print("")
-        #print(x_a_1)
-        #print(y_a_1)
-        #print(z_a_1)
-        #print(x_a_2)
-        #print(y_a_2)
-        #print(z_a_2)
-        #print(x_a_3)
-        #print(y_a_3)
-        #print(z_a_3)
+        #print("")                                                           #impresion de la lectura actual en cada uno de los tres sensores
+        #print(x_a_1)                                                        #aceleracion en x sensor 1
+        #print(y_a_1)                                                        #aceleracion en y sensor 1
+        #print(z_a_1)                                                        #aceleracion en z sensor 1
+        #print(x_a_2)                                                        #aceleracion en x sensor 2 
+        #print(y_a_2)                                                        #aceleracion en y sensor 2
+        #print(z_a_2)                                                        #aceleracion en z sensor 2
+        #print(x_a_3)                                                        #aceleracion en x sensor 3
+        #print(y_a_3)                                                        #aceleracion en y sensor 3
+        #print(z_a_3)                                                        #aceleracion en z sensor 3
         #print("")    
         
-        time.sleep(0.000001)
-        contador = contador+1
-        #base_temp_2 = time.time()
-        #print(base_temp_2-base_temp)
+        time.sleep(0.000001)                                                 #Retardo de 1 us
+        contador = contador+1                                                #incremento de ciclo de muestreo
+        #base_temp_2 = time.time()                                           #Medición de tiempo final de un ciclo de muestreo
+        #print(base_temp_2-base_temp)                                        #Impresion del tiempo requerido para muestrear un ciclo
         
     base_temp_2 = time.time()
     print(base_temp_2-base_temp)
